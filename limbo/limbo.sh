9df63f6e7306ce982bc7dd8ef4aa520a8483b09a
yuki-scratch44 <yukiscratch44@gmail.com>
2025-01-31 08:39:54 +0000

    delete files

--- limbo/limbo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while [ true ]; do
-    java -Xms1024M -Xmx1024M -jar limbo.jar
-
-    echo Server restarting...
-    echo Press CTRL + C to stop.
-done
--- velocity/velocity.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while [ true ]; do
-    java -Xms2560M -Xmx2560M -jar velocity.jar
-
-    echo Server restarting...
-    echo Press CTRL + C to stop.
-done
